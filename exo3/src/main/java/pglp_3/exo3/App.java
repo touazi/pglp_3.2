0aaf52b585872db2e09c431f74278462d68cf931
Touazi lylia <Touazi.lylia@gmail.com>
2020-03-06 14:28:31 +0100

    Suprission du main

--- exo3/src/main/java/pglp_3/exo3/App.java
+++ exo3/src/main/java/pglp_3/exo3/App.java
@@ -9,18 +9,6 @@ public class App
     public static void main( String[] args )
     {
       
-    	emplyer v=new vendeur(1500,4,"lylia",100);
-        int k=v.calcul_salaire();
-        System.out.println(k);
-        System.out.println( "Hello World!" );
-        entreprise E = new entreprise();
-        E.List_calcul.add(v);
-        System.out.println(k);
-        
-    
-       
-int j=E.Somme_Totale_Salaires();
-        System.out.println( j );
     }
 
     
